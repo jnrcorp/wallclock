79040d7abfc8170008af04771c4156592116026e
Joshua Ross <jross@Joshuas-Air.home>
2018-04-07 18:23:38 -0400

    removed unused class

--- app/src/main/java/com/a3dx2/clock/activity/SettingsActivity.java
+++ app/src/main/java/com/a3dx2/clock/activity/SettingsActivity.java
@@ -4,9 +4,6 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
@@ -15,13 +12,10 @@ import android.preference.PreferenceActivity;
 import android.support.v7.app.ActionBar;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
-import android.preference.RingtonePreference;
-import android.text.TextUtils;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
 
 import com.a3dx2.clock.R;
-import com.a3dx2.clock.service.SettingsService;
 
 import java.util.List;
 
